980dbf455d714a8dc77b18baafe1ec44d221b7f7
gabrielbadilla <gbadilla60348@ufide.ac.cr>
2024-03-13 02:10:09 -0600

    area de registro

---
+++ ZarpeOQue/src/main/java/ZarpeOQue/demo/controller/registroempleado.java
@@ -0,0 +1,29 @@
+
+package ZarpeOQue.demo.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ *
+ * @author Jonathan
+ */
+
+@Controller
+@Slf4j
+@RequestMapping("/menu")
+public class registroempleado {
+    
+    @GetMapping("/registroEmpleado")
+    public String inicio() {
+        return "/menu/registroEmpleado";
+    }
+    
+}//final de la clase
